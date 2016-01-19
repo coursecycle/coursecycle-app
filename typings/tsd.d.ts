f1ff992f1bc61d3012844a79a3e5e1e1d0923be5
Roger Chen <rogerthechen@gmail.com>
2016-01-18 21:04:50 -0800

    Add fetch for AJAX

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,8 +1,10 @@
+/// <reference path="es6-promise/es6-promise.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
 /// <reference path="react-router/history.d.ts" />
 /// <reference path="react-router/react-router.d.ts" />
 /// <reference path="react/react-dom.d.ts" />
 /// <reference path="react/react.d.ts" />
-/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="whatwg-fetch/whatwg-fetch.d.ts" />
 
 // Custom typings, contribute back to DefinitelyTyped
 /// <reference path="semantic-ui/semantic-ui-sidebar.d.ts" />
